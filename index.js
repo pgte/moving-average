3a5d25d7ff6dde4e14e2615b585e0137080a27eb
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-03-21 15:42:54 +0000

    works

---
+++ index.js
@@ -0,0 +1,42 @@
+var exp = Math.exp;
+var pow = Math.pow;
+var E = Math.E;
+
+exports =
+module.exports =
+function MovingAverage(timespan) {
+  if (typeof timespan != 'number')
+    throw new Error('must provide a timespan to the moving average constructor');
+
+  if (timespan <= 0)
+    throw new Error('must provide a timespan > 0 to the moving average constructor');
+
+  var ma;
+  var previousTime;
+  var ret = {};
+
+  
+  function alpha(t, pt) {
+    return 1 - (exp(- (t - pt) / timespan))
+  }
+
+  
+  ret.push =
+  function push(time, value) {
+    if (previousTime) {
+      var a = alpha(time, previousTime);
+      ma = a * value + (1 - a) * ma;
+    } else {
+      ma = value;
+    }
+    previousTime = time;
+  };
+
+  ret.movingAverage =
+  function movingAverage() {
+    return ma;
+  }
+
+  return ret;
+
+};
